e66b8fa06cc8dd30f2c3334c4009980998a5139d
eldisnex <ulest20@gmail.com>
2025-03-27 12:42:13 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const ul = document.querySelector('ul');
+const add = document.getElementById('add');
+const del = document.getElementById('delete');
+
+function crearElemento(nombre, id, checked = false) {
+   const li = document.createElement('li');
+   li.id = id;
+   li.textContent = nombre;
+   //    CHECKBOX
+   const checkbox = document.createElement('input');
+   checkbox.type = 'checkbox';
+   checkbox.checked = checked;
+   checkbox.addEventListener('change', () =>
+      localStorage.setItem(localStorage.key(id), checkbox.checked)
+   );
+   //    BTN
+   const btn = document.createElement('button');
+   btn.textContent = 'eliminar tarea';
+   btn.addEventListener('click', () => {
+      li.remove();
+      localStorage.removeItem(localStorage.key(id));
+   });
+   li.appendChild(checkbox);
+   li.appendChild(btn);
+   ul.appendChild(li);
+}
+
+var lastId = -1;
+
+function recargarTareas() {
+   for (let i = 0; i < localStorage.length; i++) {
+      key = localStorage.key(i);
+      crearElemento(key.split(';')[1], i, localStorage.getItem(key) === 'true');
+      lastId = i;
+   }
+}
+
+recargarTareas();
+
+add.addEventListener('click', () => {
+   var nombre = 'nuevo elemento';
+   crearElemento(nombre, ++lastId);
+   const d = new Date();
+   const dString = d.toLocaleString() + ':' + d.getMilliseconds();
+   localStorage.setItem(dString + ';' + nombre, false);
+});
+
+del.addEventListener('click', () => {
+   ul.innerText = '';
+   localStorage.clear();
+});
